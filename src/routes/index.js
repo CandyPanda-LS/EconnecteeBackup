b6be840da13dc4c37c037f5fd0aa933b4c80e999
Dilmi101 <dnimasha971@gmail.com>
2021-08-01 12:50:21 +0530

    PMlogin

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavbarComponent from "../common/NavbarComponent/NavbarComponent";
 import EmployeeLoginPage from "../pages/EmployeeLoginPage/EmployeeLoginPage";
 import AdminLoginPage from "../pages/AdminLoginPage/AdminLoginPage";
+import PMLoginPage from "../pages/PMLoginPage/PMLoginPage";
 
 export default class index extends Component {
   render() {
@@ -12,6 +13,7 @@ export default class index extends Component {
         <Router>
           <Route path="/" exact component={EmployeeLoginPage} />
           <Route path="/adminlogin" exact component={AdminLoginPage} />
+          <Route path="/pmlogin" exact component={PMLoginPage} />
         </Router>
       </div>
     );
