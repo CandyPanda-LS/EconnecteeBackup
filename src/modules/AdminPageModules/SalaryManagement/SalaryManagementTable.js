bed3431728ea3d99d3676e3b78e016330710cd3d
ayodyabanuka <ayodyabanukafernando@gmail.com>
2021-08-13 22:33:16 +0530

    completed Admin UIs

---
+++ src/modules/AdminPageModules/SalaryManagement/SalaryManagementTable.js
@@ -0,0 +1,119 @@
+import React, { Component } from "react";
+
+export default class SalaryManagementTable extends Component {
+  render() {
+    return (
+      <div>
+        <div className="card mt-3 boderRadiusCards">
+          <div className="card-body">
+            <div className="row">
+              <h5>12 Employees</h5>
+              <h6 className="text-muted">2020-07-24 to 2020-08-24</h6>
+              <hr />
+            </div>
+            <table className="table table-hover">
+              <tbody>
+                <tr>
+                  <td>
+                    <h6>E001</h6>
+                  </td>
+                  <td>
+                    <h6>Senura Jayadeva</h6>
+                  </td>
+                  <td>
+                    <h6>20000 LKR</h6>
+                  </td>
+                  <td>
+                    <button className="btn empTableBtn empProjectTableBtn">
+                      PAY
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <h6>E001</h6>
+                  </td>
+                  <td>
+                    <h6>Senura Jayadeva</h6>
+                  </td>
+                  <td>
+                    <h6>20000 LKR</h6>
+                  </td>
+                  <td>
+                    <button className="btn empTableBtn empProjectTableBtn">
+                      PAY
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <h6>E001</h6>
+                  </td>
+                  <td>
+                    <h6>Senura Jayadeva</h6>
+                  </td>
+                  <td>
+                    <h6>20000 LKR</h6>
+                  </td>
+                  <td>
+                    <button className="btn empTableBtn empProjectTableBtn">
+                      PAY
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <h6>E001</h6>
+                  </td>
+                  <td>
+                    <h6>Senura Jayadeva</h6>
+                  </td>
+                  <td>
+                    <h6>20000 LKR</h6>
+                  </td>
+                  <td>
+                    <button className="btn empTableBtn empProjectTableBtn">
+                      PAY
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <h6>E001</h6>
+                  </td>
+                  <td>
+                    <h6>Senura Jayadeva</h6>
+                  </td>
+                  <td>
+                    <h6>20000 LKR</h6>
+                  </td>
+                  <td>
+                    <button className="btn empTableBtn empProjectTableBtn">
+                      PAY
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <h6>E001</h6>
+                  </td>
+                  <td>
+                    <h6>Senura Jayadeva</h6>
+                  </td>
+                  <td>
+                    <h6>20000 LKR</h6>
+                  </td>
+                  <td>
+                    <button className="btn empTableBtn empProjectTableBtn">
+                      PAY
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
