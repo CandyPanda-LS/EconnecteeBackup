0dec1751e06a30066343b1fbf421ad8eea8d89f8
CandyPanda-LS <i19132310@my.sliit.lk>
2021-09-04 14:46:06 +0530

    salary management

--- src/modules/AdminPageModules/SalaryManagement/SalaryManagementTable.js
+++ src/modules/AdminPageModules/SalaryManagement/SalaryManagementTable.js
@@ -1,114 +1,55 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actions from "../../../store/actions/EmployeeActions";
+import ProfileImage from "../../../assets/images/profile.png";
 
-export default class SalaryManagementTable extends Component {
+import { confirmAlert } from 'react-confirm-alert'; 
+import 'react-confirm-alert/src/react-confirm-alert.css';
+
+
+class SalaryManagementTable extends Component {
+  constructor(props) {
+    super(props);
+    
+  }
+  componentDidMount() {
+    this.props.fetchAllEmployeeList();
+  }
+
   render() {
     return (
       <div>
         <div className="card mt-3 boderRadiusCards">
           <div className="card-body">
             <div className="row">
-              <h5>12 Employees</h5>
-              <h6 className="text-muted">2020-07-24 to 2020-08-24</h6>
+              <h5>{`${this.props.employeeList.length} Employees`}</h5>
+              <h6 className="text-muted">2021-08-24 to 2020-09-24</h6>
               <hr />
             </div>
             <table className="table table-hover">
               <tbody>
-                <tr>
-                  <td>
-                    <h6>E001</h6>
-                  </td>
-                  <td>
-                    <h6>Senura Jayadeva</h6>
-                  </td>
-                  <td>
-                    <h6>20000 LKR</h6>
-                  </td>
-                  <td>
-                    <button className="btn empTableBtn empProjectTableBtn">
-                      PAY
-                    </button>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <h6>E001</h6>
-                  </td>
-                  <td>
-                    <h6>Senura Jayadeva</h6>
-                  </td>
-                  <td>
-                    <h6>20000 LKR</h6>
-                  </td>
-                  <td>
-                    <button className="btn empTableBtn empProjectTableBtn">
-                      PAY
-                    </button>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <h6>E001</h6>
-                  </td>
-                  <td>
-                    <h6>Senura Jayadeva</h6>
-                  </td>
-                  <td>
-                    <h6>20000 LKR</h6>
-                  </td>
-                  <td>
-                    <button className="btn empTableBtn empProjectTableBtn">
-                      PAY
-                    </button>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <h6>E001</h6>
-                  </td>
-                  <td>
-                    <h6>Senura Jayadeva</h6>
-                  </td>
-                  <td>
-                    <h6>20000 LKR</h6>
-                  </td>
-                  <td>
-                    <button className="btn empTableBtn empProjectTableBtn">
-                      PAY
-                    </button>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <h6>E001</h6>
-                  </td>
-                  <td>
-                    <h6>Senura Jayadeva</h6>
-                  </td>
-                  <td>
-                    <h6>20000 LKR</h6>
-                  </td>
-                  <td>
-                    <button className="btn empTableBtn empProjectTableBtn">
-                      PAY
-                    </button>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <h6>E001</h6>
-                  </td>
-                  <td>
-                    <h6>Senura Jayadeva</h6>
-                  </td>
-                  <td>
-                    <h6>20000 LKR</h6>
-                  </td>
-                  <td>
-                    <button className="btn empTableBtn empProjectTableBtn">
-                      PAY
-                    </button>
-                  </td>
-                </tr>
+              {this.props.employeeList.map((singleEmployee, index) => {
+                return (
+                  <tr>
+                    <th scope="row">{++index}</th>
+                    <td>{singleEmployee.username}</td>
+                    <td>{singleEmployee.name}</td>
+                    <td>{`${singleEmployee.salary}/=`}</td>
+                    <td>
+                      <img
+                        className="img-fluid rounded-circle employeeImage"
+                        src={
+                          singleEmployee.profileImg
+                            ? singleEmployee.profileImg
+                            : ProfileImage
+                        }
+                        alt="profile image2"
+                      />
+                    </td>
+                    
+                  </tr>
+                );
+              })}
               </tbody>
             </table>
           </div>
@@ -117,3 +58,12 @@ export default class SalaryManagementTable extends Component {
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  employeeList: state.employeeReducer.employeeList,
+});
+
+const mapActionToProps = {
+  fetchAllEmployeeList: actions.fetchAllEmployeeList,
+};
+export default connect(mapStateToProps, mapActionToProps)(SalaryManagementTable);
--- src/modules/EmployeePageModules/EmployeeDashboardModule/EmployeeDashboardHeader.js
+++ src/modules/EmployeePageModules/EmployeeDashboardModule/EmployeeDashboardHeader.js
@@ -2,38 +2,36 @@ import React, { Component } from "react";
 import "./EmployeeDashboardHeader.css";
 
 export default class EmployeeDashboardHeader extends Component {
-
   render() {
     return (
       <div>
         {this.props.currentEmployee ? (
           <div className="mt-3">
             <div className="row">
-              
               <div className="col-lg-3 mt-1">
-                
-                  <div className="card-body">
-                    <div className="row p-2">
-                      <div className="col empDashboardUserDetailsTwo mt-2">
-                        <span className="display-5 fw-bold " >{`Hello!`}</span>
-                        <h3>{this.props.currentEmployee.name}</h3>
+                <div className="card-body">
+                  <div className="row p-2">
+                    <div className="col empDashboardUserDetailsTwo mt-2">
+                      <span className="display-5 fw-bold ">{`Hello!`}</span>
+                      <h3>{this.props.currentEmployee.name}</h3>
+                      <h6 className="text-muted">
+                        {`${this.props.currentEmployee.username} - ${this.props.currentEmployee.department}`}
+                      </h6>
+                      <h6 style={{ color: "#087E8B" }}>
+                        {`Salary for Month : ${this.props.currentEmployee.salary}/=`}
+                      </h6>
+                      {this.props.currentEmployee.attendanceList &&
+                      this.props.currentEmployee.attendanceList.length > 0 ? (
                         <h6 className="text-muted">
-                          {`${this.props.currentEmployee.username} - ${this.props.currentEmployee.department}`}
+                          {`Working Days - ${this.props.currentEmployee.attendanceList.length}`}
                         </h6>
-                        <h6  style={{color: "#087E8B"}}>
-                          Salary for Month : 35 000/=
-                        </h6>
-                        <h6 className="text-muted">
-                          Working Days - 28
-                        </h6>
-                        <h6 className="text-muted">
-                          Current Leaves - 3
-                        </h6>
-                      </div>
+                      ) : (
+                        ""
+                      )}
                     </div>
                   </div>
+                </div>
               </div>
-              
             </div>
           </div>
         ) : (
